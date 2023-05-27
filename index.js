ae9d3606df9f249d285b8e4ad24debd3f3a7c767
Volodymyr-Ostrohliad <willhonorodua@gmail.com>
2023-05-27 21:50:53 +0300

    fix2

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let hours = prompt("Введіть кількість годин від 1.");
-console.log(hours.trim())
+console.log(hours)
 if (hours === null) {
   alert('Шкода, що ви не захотіли ввести кількість годин.');
-  console.log(hours)
+  // console.log(hours)
 }
 else if (isNaN(hours) || hours.trim() === '' || hours === '0') {
   while (isNaN(hours) || hours.trim() === '' || hours === '0') {
     hours = prompt('Помилка! Ви ввели не число, або 0, або залишили поле пустим. Будь ласка, введіть число від 1.');
-    console.log(hours.trim())
+    console.log(hours)
     if (hours === null) {
       alert('Шкода, що ви не захотіли ввести клькість годин.');
       console.log(hours)
